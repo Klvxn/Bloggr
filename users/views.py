8e86d100de2560abefa4470d695f13b281f9381f
Achifa <akpulukelvin@gmail.com>
2023-01-12 19:46:50 +0100

    meh

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import abort, Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from database.db import db
